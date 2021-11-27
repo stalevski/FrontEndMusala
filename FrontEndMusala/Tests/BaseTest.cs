487f2a07c364ecd89207507b5873809e81adbb4f
stalevski <talevski95@gmail.com>
2021-11-27 14:40:46 +0100

    initial push

---
+++ FrontEndMusala/Tests/BaseTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace FrontEndMusala.Tests
+{
+    public class BaseTest
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+
+    }
+}
