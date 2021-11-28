0ad09ab53bbf16df50ffbb958b68863d8ca62dad
stalevski <talevski95@gmail.com>
2021-11-28 16:09:53 +0100

    update packages

--- FrontEndMusala/Tests/Test.cs
+++ FrontEndMusala/Tests/Test.cs
@@ -1,13 +1,34 @@
-﻿using NUnit.Framework;
+﻿using FrontEndMusala.Pages;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace FrontEndMusala.Tests
 {
+    [TestFixture]
     public class Test : BaseTest
     {
+        IWebDriver driver;
+        //public string MusalaUrl = ConfigurationManager.AppSettings["MusalaHomePageUrl"];
+
+        [SetUp]
+        public void Init()
+        {
+            driver = new ChromeDriver();
+            driver.Url = "https://www.musala.com/";
+        }
+
         [Test]
         public static void FirstTest()
         {
+            HomePage homePage = new HomePage(driver);
+            
+        }
 
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
         }
     }
 }
